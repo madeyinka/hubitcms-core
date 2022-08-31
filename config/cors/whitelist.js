ed643009d75135954daf770022953e5a71592013
madeyinka <mutairadeyinka@gmail.com>
2022-08-31 11:36:26 +0100

    whitelisted

--- config/cors/whitelist.js
+++ config/cors/whitelist.js
@@ -3,7 +3,7 @@ const whiteListed = [
     'http://localhost:3001',
     'http://localhost:3002',
     'http://localhost:60005',
-    'https://hubit-dashboard.vercel.app/'
+    'https://hubit-dashboard.vercel.app'
 ]
 
 module.exports = whiteListed
