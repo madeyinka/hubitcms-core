99bb4e944f9a9fb5483d0a35f9afe760c1bd6584
madeyinka <mutairadeyinka@gmail.com>
2022-08-31 10:05:58 +0100

    whitelisted

--- config/cors/whitelist.js
+++ config/cors/whitelist.js
@@ -2,7 +2,8 @@ const whiteListed = [
     'http://localhost:3000',
     'http://localhost:3001',
     'http://localhost:3002',
-    'http://localhost:60005'
+    'http://localhost:60005',
+    'https://github.com/madeyinka/hubitcms-core.git'
 ]
 
 module.exports = whiteListed
