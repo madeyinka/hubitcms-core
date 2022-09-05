1f84c58325efd8fcc3b308dc2c022b3f864f9f9c
madeyinka <mutairadeyinka@gmail.com>
2022-09-05 22:05:50 +0100

    updates

--- routes/public/post.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const post = require('../../controllers/public/post.controller')
-
-router.get('/posts', post.getPosts)
-
-module.exports = router
