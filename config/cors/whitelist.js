29d0400de697171b888f4ce4ec7415fd12611d8f
madeyinka <mutairadeyinka@gmail.com>
2022-09-07 05:16:45 +0100

    update-config

--- config/cors/whitelist.js
+++ config/cors/whitelist.js
@@ -5,8 +5,8 @@ const whiteListed = [
     'http://localhost:60005',
     'https://hubit-dashboard.vercel.app',
     'https://enugu-monitor.vercel.app',
-    'https://www.enugumonitor.com/',
-    'https://enugumonitor.com/'
+    'https://www.enugumonitor.com',
+    'https://enugumonitor.com'
 ]
 
 module.exports = whiteListed
