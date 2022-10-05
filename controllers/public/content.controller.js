1c0efcc893a0f41de40d629cc8dde8aa40d3f2c8
madeyinka <mutairadeyinka@gmail.com>
2022-10-05 10:39:42 +0100

    public-api-update

--- controllers/public/content.controller.js
+++ controllers/public/content.controller.js
@@ -62,6 +62,70 @@ const ContentInit = {
                 error:{}
             })
         }
+    },
+
+    getComponents: async (req, res) => {
+        const user = req.query.user
+        try {
+            const content = await ContentModel.findOne({user:user})
+            if (!content) {
+                return res.status(404).json({
+                    success:false,
+                    message:"content not found",
+                    error:{
+                        statusCode:404,
+                        description:"Content not found."
+                    }
+                })
+            } else {
+                return res.status(200).json({
+                    success:true,
+                    message:"Data found",
+                    data: {
+                        statusCode:200,
+                        components: content.components
+                    }
+                })
+            }
+        } catch(err) {
+            return res.status(400).json({
+                success:false,
+                message:err.message,
+                error:{}
+            })
+        }
+    },
+
+    getServices: async (req, res) => {
+        const user = req.query.user
+        try {
+            const content = await ContentModel.findOne({user:user})
+            if (!content) {
+                return res.status(404).json({
+                    success:false,
+                    message:"content not found",
+                    error:{
+                        statusCode:404,
+                        description:"Content not found."
+                    }
+                })
+            } else {
+                return res.status(200).json({
+                    success:true,
+                    message:"Data found",
+                    data: {
+                        statusCode:200,
+                        services: content.services
+                    }
+                })
+            }
+        } catch(err) {
+            return res.status(400).json({
+                success:false,
+                message:err.message,
+                error:{}
+            })
+        }
     }
 }
 
--- routes/public/content.js
+++ routes/public/content.js
@@ -4,5 +4,7 @@ const content = require('../../controllers/public/content.controller')
 
 router.get('/posts', content.getPosts)
 router.get('/categories', content.getCategories)
+router.get('/components', content.getComponents)
+router.get('/services', content.getServices)
 
 module.exports = router
